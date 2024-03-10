2fdd500994043d48efc4369589e558fc0a238b36
PinG <1552014195@qq.com>
2024-03-11 01:03:17 +0800

    最终锁死版

--- src/homework1/FileIO.java
+++ src/homework1/FileIO.java
@@ -11,8 +11,10 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class FileIO {
-    private static final Lock lock = new ReentrantLock();
-    static Data search(String id,String filename) {
+        private static final Lock lock = new ReentrantLock();
+
+    static Data search(String id, String filename) {
+        lock.lock();
         try {
             var in = Files.newInputStream(Path.of("./src/homework1/" + filename));
             var scanner = new Scanner(in);
@@ -21,6 +23,7 @@ public class FileIO {
                 var file_id = scanner.next();
                 var file_money = scanner.nextDouble();
                 if (file_id.equals(id)) {
+                    lock.unlock();
                     return new Data(id, file_money);
                 }
             }
@@ -28,13 +31,14 @@ public class FileIO {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        lock.unlock();
         return null;
     }
-    static void record(Data data,String filename){
+
+        static void record(Data data,String filename){
+        lock.lock();
         var path = Path.of("./src/homework1/" + filename);
         try {
-            lock.lock();
-
             List<Data> all_data = new ArrayList<>();
 
             var in = Files.newInputStream(path);
@@ -59,7 +63,6 @@ public class FileIO {
 
             var out = Files.newOutputStream(path);
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
-
             // 创建 BufferedWriter 对象，用于缓冲字符写入
             BufferedWriter writer = new BufferedWriter(outputStreamWriter);
 
@@ -68,10 +71,50 @@ public class FileIO {
                 writer.write("\n");
             }
             writer.close();
-
-            lock.unlock();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        lock.unlock();
     }
+//    public static void record(Data data, String filename) {
+//        var path = Path.of("./src/homework1/" + filename);
+//        try {
+//
+//                List<Data> all_data = new ArrayList<>();
+//
+//                try (var in = Files.newInputStream(path);
+//                     var scanner = new Scanner(in)) {
+//                    var is_found = false;
+//                    while (scanner.hasNext()) {
+//                        var file_id = scanner.next();
+//                        var file_money = scanner.nextDouble();
+//
+//                        if (file_id.equals(data.getId())) {
+//                            all_data.add(data);
+//                            is_found = true;
+//                        } else {
+//                            all_data.add(new Data(file_id, file_money));
+//                        }
+//                    }
+//
+//                    if (!is_found) {
+//                        all_data.add(data);
+//                    }
+//                }
+//            synchronized (lock) {
+//                try (var out = Files.newOutputStream(path);
+//                     OutputStreamWriter outputStreamWriter = new OutputStreamWriter(out);
+//                     BufferedWriter writer = new BufferedWriter(outputStreamWriter)) {
+//                    Thread.sleep(3000);
+//
+//                    for (var i : all_data) {
+//                        writer.write(i.getId() + " " + i.getMoney());
+//                        writer.write("\n");
+//                    }
+//                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
